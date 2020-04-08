d66bddc8fe07dc35f8dcd37f27efe5ea528b7df7
qhuy2301 <huyqle0123@utexas.edu>
2020-04-08 01:05:02 -0500

    insert method implemented

--- iter/IterSkiplist.java
+++ iter/IterSkiplist.java
@@ -3,15 +3,15 @@ package iter;
 import java.util.ArrayList;
 
 public class IterSkiplist {
-    
-    /* 
+
+    /*
     Other Details:
     Allow duplicates
     */
 
     class SkipListNode {
         Integer val;
-        
+
         SkipListNode left;
         SkipListNode right;
         SkipListNode up;
@@ -37,16 +37,46 @@ public class IterSkiplist {
         tail = new SkipListNode(Integer.MAX_VALUE);
         size = 0;
         maxHeight = 0;
+        head.right = tail;
+        tail.left = head;
     }
 
     /* Huy Le */
     public boolean insert(Integer a) {
-        return false;
+        if (isPresent(a)) return true;
+        size++;
+
+        level = 0;
+
+        while (Math.random() > 0.5) level++;
+        while (level > maxHeight) {
+            head.right.insert(null);
+            maxHeight++;
+        }
+        SkiplistNode p = new SkipListNode(a);
+        SkipListNode current;
+        while (level >= 0) {
+            current = head;
+            while (current.val != Integer.MAX_VALUE) {
+                if (current.right.val < a) current = current.right;
+                else {
+                    p.right = current.right;
+                    p.left = current;
+                    current.right.left = p;
+                    current.right = p;
+                    if (level != maxHeight) p.up = p;
+                    if (level != 0) p.down = p;
+                    break;
+                }
+            }
+            level--;
+        }
+        return true;
     }
 
     /* Sammy */
     /**
-     * Deletes an entire tower with the input value and returns true. 
+     * Deletes an entire tower with the input value and returns true.
      * If the input value is not in any Node, then the function returns false.
      * @param a Target value of the Node to delete from SkipList
      * @return boolean
@@ -80,8 +110,8 @@ public class IterSkiplist {
         return false;
     }
 
-    /* 
-    Returns # of elems 
+    /*
+    Returns # of elems
     */
     public Integer size() {
         return size;
@@ -97,10 +127,10 @@ public class IterSkiplist {
     }
 
     /**
-     * This method returns the SkipListNode with a value that matches 
-     * the target. If there are duplicate values in the SkipList, 
-     * this will return the lowest SkipListNode in the tallest tower. 
-     * If the value does not exist in the SkipList, then the method 
+     * This method returns the SkipListNode with a value that matches
+     * the target. If there are duplicate values in the SkipList,
+     * this will return the lowest SkipListNode in the tallest tower.
+     * If the value does not exist in the SkipList, then the method
      * will return the node with the largest value less than the param target value.
      * @param target Integer value of Node to find
      * @return SkipListNode
