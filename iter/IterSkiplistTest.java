7e8559dc7003456f4d22f93bcb36faca163b79e4
David Chao <chao.david@utexas.edu>
2020-04-14 18:34:26 -0500

    finished the tests

--- iter/IterSkiplistTest.java
+++ iter/IterSkiplistTest.java
@@ -121,54 +121,58 @@ public class IterSkiplistTest {
             while(!my.contains(r)){
                 r = rand.nextInt(5000);
             }
-            if(!myList.delete(r)){
-                assertTrue(myList.isPresent(r));
-                myList.delete(r);
-            }
+            my.remove(r);
+            assertTrue(myList.delete(r));
         }
         assertTrue(myList.size==500);
         myList.printg();
 
 
     }
-
-//        myList.insert(5); //[5]
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        System.out.println("List: ");
-//        myList.printg();
-//        myList.insert(9); //[5, 9]
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        System.out.println("List: ");
-//        myList.printg();
-//        myList.insert(2);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        myList.insert(7); //[2, 5, 7, 9]
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        System.out.println("List: ");
-//        myList.printg();
-//        myList.insert(1);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        myList.insert(3);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        myList.insert(4);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        myList.insert(6);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        myList.insert(8);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        myList.insert(10); //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        System.out.println("List: ");
-//        myList.printg();
-//        System.out.println("4 is in the list " + myList.isPresent(4));
-//        System.out.println("List: ");
-//        myList.printg();
-//        myList.delete(4);
-//        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
-//        System.out.println("List: ");
-//        myList.printg();
-//        System.out.println("4 is in the list " + myList.isPresent(4));
-//        System.out.println("Size is " + myList.size());
-//        System.out.println("Height is " + myList.height());
-//    }
+    @Test
+    public void printingTest() {
+        IterSkiplist myList= new IterSkiplist();
+        System.out.println("List: ");
+        myList.printg(); //should print an empty list
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        System.out.println("20 is in the list " + myList.isPresent(20));
+        myList.insert(5); //[5]
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        System.out.println("List: ");
+        myList.printg();
+        myList.insert(9); //[5, 9]
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        System.out.println("List: ");
+        myList.printg();
+        myList.insert(2);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        myList.insert(7); //[2, 5, 7, 9]
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        System.out.println("List: ");
+        myList.printg();
+        myList.insert(1);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        myList.insert(3);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        myList.insert(4);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        myList.insert(6);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        myList.insert(8);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        myList.insert(10); //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        System.out.println("List: ");
+        myList.printg();
+        System.out.println("4 is in the list " + myList.isPresent(4));
+        System.out.println("List: ");
+        myList.printg();
+        myList.delete(4);
+        System.out.println("List Height: " + myList.height() + "; List size: " + myList.size());
+        System.out.println("List: ");
+        myList.printg();
+        System.out.println("4 is in the list " + myList.isPresent(4));
+        System.out.println("Size is " + myList.size());
+        System.out.println("Height is " + myList.height());
+    }
 }
