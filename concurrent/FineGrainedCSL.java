ce34f71ecd28cc267fa558d9ce0a5fc4e0bae30f
David Chao <chao.david@utexas.edu>
2020-04-14 21:42:56 -0500

    added in some basic code

--- concurrent/FineGrainedCSL.java
+++ concurrent/FineGrainedCSL.java
@@ -5,7 +5,11 @@ public class FineGrainedCSL<T> {
     final static int MAX_LEVEL = 32;
     final FineGrainedCSLNode<T> head = new FineGrainedCSLNode<T>(Integer.MIN_VALUE);
     final FineGrainedCSLNode<T> tail = new FineGrainedCSLNode<T>(Integer.MAX_VALUE);
-
+    public LazySkipList() {
+        for (int i = 0; i < head.next.length; i++) {
+            head.next[i] = tail;
+        }
+    }
 
     
 }
